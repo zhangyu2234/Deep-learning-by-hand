f1956ce6d22509b8746f100b2baf01574c47efb6
zhangyu2234 <46471060+zhangyu2234@users.noreply.github.com>
2024-04-09 14:47:11 +0800

    Add files via upload

---
+++ utils/Common.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+
+from random import shuffle
+import torch
+
+
+class DataLoader:
+    def __init__(self, data, labels, batch_size, shuffle=False):
+        self.data = data
+        self.batch_size = batch_size
+        self.labels = labels
+        self.indexes = list(range(len(self.data)))
+        self.shuffle = shuffle
+    def __iter__(self):
+        self.start = 0
+        if self.shuffle is True:
+            shuffle(self.indexes)
+        return self
+    def __next__(self):
+        start = self.start
+        self.end = min(start+self.batch_size, len(self.data))
+        if start >= self.end:
+            raise StopIteration
+        self.start += self.batch_size
+        index = self.indexes[start:self.end]
+        return self.data[index], self.labels[index]
+
+
+def loss(y_hat, y):
+    return torch.mean((y_hat.reshape(y.shape) - y)**2 / 2)
+
+
+def sgd(params, lr, batch_size):
+    with torch.no_grad():
+        if isinstance(params, list):
+            for i in range(len(params)):
+                params[i] -= lr * params[i].grad / batch_size
+                params[i].grad.zero_()
+        else:
+            params -= lr * params.grad / batch_size
+            params.grad.zero_()
+
+def relu(x):
+    a = torch.zeros(x.shape)
+    return torch.max(a, x)
+
+
